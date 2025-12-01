db55217f090309cc119a365ad0d1dad1f7a6b063
Julian Velasco <juliancamilovelasco@gmail.com>
2025-12-01 16:08:28 -0500

    code

--- qa-agentic-framework-ts/jest.config.cjs
+++ qa-agentic-framework-ts/jest.config.cjs
@@ -1,26 +1,6 @@
-// /** @type {import('ts-jest').JestConfigWithTsJest} */
-// export default {
-//   preset: 'ts-jest/presets/default-esm', // Soporte para ES Modules
-//   testEnvironment: 'node',
-//   transform: {
-//     '^.+\\.ts$': ['ts-jest', { useESM: true }]
-//   },
-//   extensionsToTreatAsEsm: ['.ts'],
-//   globals: {
-//     'ts-jest': {
-//       useESM: true
-//     }
-//   },
-//   moduleFileExtensions: ['ts', 'js', 'json'],
-//   testMatch: ['**/__tests__/**/*.ts', '**/?(*.)+(spec|test).ts'],
-//   verbose: true
-// };
-
-
-
 /** @type {import('ts-jest').JestConfigWithTsJest} */
-module.exports = {
-  preset: 'ts-jest/presets/default-esm',
+export default {
+  preset: 'ts-jest/presets/default-esm', // Soporte para ES Modules
   testEnvironment: 'node',
   transform: {
     '^.+\\.ts$': ['ts-jest', { useESM: true }]
